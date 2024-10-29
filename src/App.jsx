bc3c06e4214eff4fe27276fc1e056479be081900
Paulo Victor Lemos de Almeida <paulovictor1918@gmail.com>
2024-10-29 11:18:38 -0300

    feat: lesson about properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Post } from "./Post";
+
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Autor 1"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, blanditiis. Nihil consequatur rerum itaque, eius modi nam culpa impedit laboriosam labore! Ipsam suscipit ducimus quam repudiandae libero voluptate nihil vel."
+      />
+      <Post
+        author="Autor 2"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, blanditiis. Nihil consequatur rerum itaque, eius modi nam culpa impedit laboriosam labore! Ipsam suscipit ducimus quam repudiandae libero voluptate nihil vel."
+      />
     </div>
   );
 }
