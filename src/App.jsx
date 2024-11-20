66c1bb5a2e56e0c3d132c73a4daaaa46ff4e7acd
Paulo301 <paulovictor1918@gmail.com>
2024-11-20 20:16:03 -0300

    feat: lesson using key prop

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,38 @@ const posts = [
   {
     id: 1,
     author: {
-      avatarUrl: 'https://github.com/paulo301.png',
-      name: 'Paulo Victor',
-      role: 'Programador'
+      avatarUrl: "https://github.com/paulo301.png",
+      name: "Paulo Victor",
+      role: "Programador",
     },
     content: [
-      { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare' },
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
     ],
-    publishedAt: new Date('2024-11-20 10:00:00'),
+    publishedAt: new Date("2024-11-20 10:00:00"),
   },
   {
     id: 2,
     author: {
-      avatarUrl: 'https://github.com/maykbrito.png',
-      name: 'Mayk Brito',
-      role: 'Educator @Rocketseat'
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat",
     },
     content: [
-      { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare' },
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
     ],
-    publishedAt: new Date('2024-11-19 10:00:00'),
+    publishedAt: new Date("2024-11-19 10:00:00"),
   },
 ];
 
@@ -45,14 +53,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          {posts.map(post => {
+          {posts.map((post) => {
             return (
               <Post
+                key={post.id}
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}
               />
-            )
+            );
           })}
         </main>
       </div>
